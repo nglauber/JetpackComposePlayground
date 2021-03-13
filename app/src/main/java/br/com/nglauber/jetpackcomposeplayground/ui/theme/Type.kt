5e519c344c3bfbf55c4c26dcbf7608fb66ee0ad2
Nelson Glauber <nglaubervasc@gmail.com>
2021-03-13 14:24:53 -0300

    Adding custom font

--- app/src/main/java/br/com/nglauber/jetpackcomposeplayground/ui/theme/Type.kt
+++ app/src/main/java/br/com/nglauber/jetpackcomposeplayground/ui/theme/Type.kt
@@ -1,28 +1,21 @@
 package br.com.nglauber.jetpackcomposeplayground.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
+private val defaultTypography = Typography()
 val typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    h1 = defaultTypography.h1.copy(fontFamily = appFontFamily),
+    h2 = defaultTypography.h2.copy(fontFamily = appFontFamily),
+    h3 = defaultTypography.h3.copy(fontFamily = appFontFamily),
+    h4 = defaultTypography.h4.copy(fontFamily = appFontFamily),
+    h5 = defaultTypography.h5.copy(fontFamily = appFontFamily),
+    h6 = defaultTypography.h6.copy(fontFamily = appFontFamily),
+    subtitle1 = defaultTypography.subtitle1.copy(fontFamily = appFontFamily),
+    subtitle2 = defaultTypography.subtitle2.copy(fontFamily = appFontFamily),
+    body1 = defaultTypography.body1.copy(fontFamily = appFontFamily),
+    body2 = defaultTypography.body2.copy(fontFamily = appFontFamily),
+    button = defaultTypography.button.copy(fontFamily = appFontFamily),
+    caption = defaultTypography.caption.copy(fontFamily = appFontFamily),
+    overline = defaultTypography.overline.copy(fontFamily = appFontFamily)
 )

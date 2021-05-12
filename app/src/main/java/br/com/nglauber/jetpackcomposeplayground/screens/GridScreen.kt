498ca4f92734d70eef86a312b1f96b3bddb8a2b8
Nelson Glauber <nglaubervasc@gmail.com>
2021-05-12 12:56:27 -0300

    Adding state to a grid

--- app/src/main/java/br/com/nglauber/jetpackcomposeplayground/screens/GridScreen.kt
+++ app/src/main/java/br/com/nglauber/jetpackcomposeplayground/screens/GridScreen.kt
@@ -2,14 +2,15 @@ package br.com.nglauber.jetpackcomposeplayground.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -20,24 +21,34 @@ import br.com.nglauber.jetpackcomposeplayground.R
 @ExperimentalFoundationApi
 @Composable
 fun GridScreen() {
+    val list = (0..100).toList()
+    val visible = remember {
+        mutableStateListOf(*list.map { true }.toTypedArray())
+    }
     LazyVerticalGrid(
         modifier = Modifier.fillMaxSize(),
         cells = GridCells.Fixed(3),
         contentPadding = PaddingValues(8.dp)
     ) {
-        items(100) {
+        itemsIndexed(list) { index, it ->
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Image(
-                    painter = painterResource(
-                        when (it % 3) {
-                            0 -> R.drawable.icon_facebook
-                            1 -> R.drawable.icon_twitter
-                            else -> R.drawable.icon_insta
-                        }
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier.size(96.dp)
-                )
+                val itemSize = Modifier.size(96.dp)
+                if (visible[index]) {
+                    Image(
+                        painter = painterResource(
+                            when (it % 3) {
+                                0 -> R.drawable.icon_facebook
+                                1 -> R.drawable.icon_twitter
+                                else -> R.drawable.icon_insta
+                            }
+                        ),
+                        contentDescription = null,
+                        modifier = itemSize
+                    )
+                } else {
+                    Spacer(itemSize)
+                }
+                Checkbox(checked = visible[index], { visible[index] = it })
                 Text("Item $it")
             }
         }

ee7eed8a64d4cc1f0f6f90cd7ae6bed331ff9ce6
Nelson Glauber <nglaubervasc@gmail.com>
2022-05-10 00:14:17 -0300

    Adding Clipboard action to ConstraintLayout sample

--- app/src/main/java/br/com/nglauber/jetpackcomposeplayground/screens/ConstraintLayoutScreen.kt
+++ app/src/main/java/br/com/nglauber/jetpackcomposeplayground/screens/ConstraintLayoutScreen.kt
@@ -1,48 +1,77 @@
 package br.com.nglauber.jetpackcomposeplayground.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun ConstraintLayoutScreen() {
-    ConstraintLayout(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+    val clipboardManager = LocalClipboardManager.current
+    val context = LocalContext.current
+    var text by remember {
+        mutableStateOf("")
+    }
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         val (text1Ref, edit1Ref, btn1Ref, btn2Ref) = createRefs()
-        Text("Nome", modifier = Modifier.constrainAs(text1Ref) {
-            top.linkTo(parent.top)
-            centerHorizontallyTo(parent)
-        })
+        Text(
+            "Nome",
+            Modifier.constrainAs(text1Ref) {
+                top.linkTo(parent.top)
+                centerHorizontallyTo(parent)
+            }
+        )
         TextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Name") },
-            modifier = Modifier.padding(top = 8.dp)
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Digite seu nome") },
+            modifier = Modifier
+                .padding(top = 8.dp)
                 .constrainAs(edit1Ref) {
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                     top.linkTo(text1Ref.bottom)
                 })
-        Button(onClick = {},
-            content = { Text("OK") },
-            modifier = Modifier.padding(top = 8.dp).constrainAs(btn1Ref) {
-                end.linkTo(edit1Ref.end)
-                top.linkTo(edit1Ref.bottom)
-            }
-        )
-        TextButton(onClick = {},
-            content = { Text("Cancel") },
-            modifier = Modifier.padding(end = 8.dp)
+        Button(
+            onClick = {
+                clipboardManager.setText(AnnotatedString("Some text"))
+                Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .constrainAs(btn1Ref) {
+                    end.linkTo(edit1Ref.end)
+                    top.linkTo(edit1Ref.bottom)
+                }
+        ) {
+            Text("Copy")
+        }
+        TextButton(
+            onClick = {
+                text = clipboardManager.getText()?.toString() ?: ""
+            },
+            modifier = Modifier
+                .padding(end = 8.dp)
                 .constrainAs(btn2Ref) {
                     end.linkTo(btn1Ref.start)
                     baseline.linkTo(btn1Ref.baseline)
                 }
-        )
+        ) {
+            Text("Paste")
+        }
     }
 }
